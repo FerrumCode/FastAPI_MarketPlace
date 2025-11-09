87c6622fe44f0923344e7c7f0fdcae42d2bb41cf
Egor <rezonbox@gmail.com>
2025-11-09 16:35:41 +0500

    Decimal fixs

--- orders_service/app/models/order_item.py
+++ orders_service/app/models/order_item.py
@@ -11,31 +11,11 @@ from app.models.base import Base
 class OrderItem(Base):
     __tablename__ = "order_items"
 
-    id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        primary_key=True,
-        default=uuid.uuid4
-    )
-
-    order_id: Mapped[uuid.UUID] = mapped_column(
-        ForeignKey("orders.id", ondelete="CASCADE"),
-        nullable=False
-    )
-
-    product_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        nullable=False
-    )
-
-    quantity: Mapped[int] = mapped_column(
-        Integer,
-        nullable=False
-    )
-
-    unit_price: Mapped[Decimal] = mapped_column(
-        Numeric(10, 2),
-        nullable=False
-    )
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
+    order_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("orders.id", ondelete="CASCADE"),nullable=False)
+    product_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer,nullable=False)
+    unit_price: Mapped[Decimal] = mapped_column(Numeric(10, 2),nullable=False)
 
     order: Mapped["Order"] = relationship(
         back_populates="items"
